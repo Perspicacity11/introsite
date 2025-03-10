f6df9b4ecda112b46c7c88ef2756d31fc60495eb
Perspicacity11 <cassius.cr.naylor@gmail.com>
2025-03-10 16:06:33 +0000

    HTML/CSS main work complete, ready to deploy

---
+++ Intro-site/index.js
@@ -0,0 +1,1 @@
+// IDEA: have a button (in the nab bar) that when clicked, changes the entire website from 'dark mode' to 'light mode' and vice versa (this should change the background and text colours, and also the image?)
